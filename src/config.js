7ee9fa99489f6c490807d256e8d5cec77801d76d
Garrett James <garrettjames55@gmail.com>
2020-08-03 17:59:55 -0400

    update package.json

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://Kein@localhost/getsu',
-  JWT_SECRET: process.env.JWT_SECRET || 'getsu-jwt-secret',
+  DATABASE_URL:
+    process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/blogful',
+  JWT_SECRET: process.env.JWT_SECRET,
 };
